a729ce3d11385b7d976d212fbfc0a23d376ce6de
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2025-05-24 23:09:16 +0200

    Add universal icon

---
+++ src/components/LucideIcon.vue
@@ -0,0 +1,20 @@
+<template>
+    <component :is="icon" :size="size" :color="color" :stroke-width="strokeWidth" :default-class="defaultClass" />
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import * as icons from 'lucide-vue-next';
+
+interface LucideIconProps {
+    name: keyof typeof icons;
+    size?: number;
+    color?: string;
+    strokeWidth?: number;
+    defaultClass?: string;
+}
+
+const props = defineProps<LucideIconProps>()
+
+const icon = computed(() => icons[props.name]);
+</script>
